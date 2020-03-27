53f8e8eca34fa2fee097d2987b8c9c74e2ec1f87
Andrew Luckett <andyluckett9@gmail.com>
2020-03-27 19:07:45 +0000

    Initial commit

---
+++ ASGame/app/src/main/java/andrew/DDC/front/MainActivity.java
@@ -0,0 +1,114 @@
+package andrew.DDC.front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import andrew.DDC.R;
+
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefs = this.getSharedPreferences("scoredata", Context.MODE_PRIVATE);
+
+
+        refresh();
+
+        final Intent i = new Intent(MainActivity.this, Game.class);
+        Button btn = findViewById(R.id.goconvert);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("id",0);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btn = findViewById(R.id.goadd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i.putExtra("id",1);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btn = findViewById(R.id.resetconvert);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset(0);
+            }
+        });
+
+        btn = findViewById(R.id.resetadd);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset(1);
+            }
+        });
+
+    }
+
+    public void refresh() {
+        int highscore = prefs.getInt("highscore_convert", 0); //0 is the default value
+
+        TextView scorebox = findViewById(R.id.highscoreconvert);
+        scorebox.setText(getString(R.string.toaa));
+        scorebox.setText("High Score: " + highscore + ".");
+
+        highscore = prefs.getInt("highscore_add", 0); //0 is the default value
+
+        scorebox = findViewById(R.id.highscoreadd);
+        scorebox.setText("High Score: " + highscore + ".");
+    }
+
+    public void reset(final int game){
+        final AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage("Are you sure you want to reset your score?");
+        dlgAlert.setTitle("Reset");
+        dlgAlert.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences.Editor editor = prefs.edit();
+                        if (game == 0) {
+                            editor.putInt("highscore_convert", 0);
+                        }else if(game == 1){
+                            editor.putInt("highscore_add", 0);
+                        }
+                        editor.apply();
+                        refresh();
+                    }
+                });
+
+        dlgAlert.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+
+
+        refresh();
+    }
+
+}
