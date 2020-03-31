433c6dd918ce653d54d558eeda07c567acb6b423
Andrew Luckett <andyluckett9@gmail.com>
2020-03-31 22:31:31 +0100

    Remove remnants of Binary Game 2015

--- ASGame/app/src/main/java/andrew/DDC/front/MainActivity.java
+++ ASGame/app/src/main/java/andrew/DDC/front/MainActivity.java
@@ -2,19 +2,8 @@ package andrew.DDC.front;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import andrew.DDC.R;
-
 public class MainActivity extends AppCompatActivity {
+    /**
     private SharedPreferences prefs;
 
     @Override
@@ -109,5 +98,5 @@ public class MainActivity extends AppCompatActivity {
 
         refresh();
     }
-
+**/
 }
